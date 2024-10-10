cbf5951d319a02ff052e4315f94885744541fd66
AddiSC2 <chavezaddi12@gmail.com>
2024-10-10 15:19:14 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

---
+++ Fundamentos-Python/tipos_de_datos_ejercicio.py
@@ -0,0 +1,31 @@
+'''
+Addi Toro Chavez
+9 de octubre de 2024.
+Descripción:
+ejercicio formativo de tipo de datos
+'''
+
+dia="lunes"
+pasaje=20
+comida= 135.5
+otros_gastos=34.5
+presupuesto= True
+
+print("Dia: ",dia)
+print("Pasaje: ",pasaje)
+print("comida: ",comida)
+print("Otros gastos: ",otros_gastos)
+print("¿Fue mayor mi presupuesto?",presupuesto)
+
+print()
+dia_2="Martes"
+pasaje_2=12
+comida_2= 45.5
+otros_gastos_2=4.5
+presupuesto_2= False
+
+print("Dia: ",dia_2)
+print("Pasaje: ",pasaje_2)
+print("comida: ",comida_2)
+print("Otros gastos: ",otros_gastos_2)
+print("¿Fue mayor mi presupuesto?",presupuesto_2)
