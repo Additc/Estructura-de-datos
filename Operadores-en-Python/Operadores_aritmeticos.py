1a54370a701c45d5c5da78c4c10b51a201844fa1
AddiSC2 <chavezaddi12@gmail.com>
2024-10-15 15:54:50 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

---
+++ Operadores-en-Python/Operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+'''
+Addi Toro Chavez
+15 de octubre de 2024.
+Descripción:
+Ejercicio de primera clase, de introducción al lenguaje de programación python
+'''
+#operadoress aritméticos
+número1=int(input("ingrese el primer número entero: "))
+número2= int(input("ingrese el segundo número entero: "))
+
+
+print(f"la suma del {número1} y del {número2} es: {número1 + número2}")
+print(f"la resta del {número1} y del {número2} es: {número1 - número2}")
+print(f"la multiplicación del {número1} y del {número2} es: {número1 * número2}")
+print(f"la división del {número1} y del {número2} es: {número1 / número2}")
+print(f"el modulo del {número1} y del {número2} es: {número1 % número2}")
+print(f"la  del {número1} y del {número2} es: {número1 // número2}")
+print(f"la elevación del {número1} y del {número2} es: {número1 ** número2}")
+
---
+++ Operadores-en-Python/operadores_asignación.py
@@ -0,0 +1,25 @@
+'''
+Addi Toro Chavez
+15 de octubre de 2024.
+Descripción:
+Ejercicio de primera clase, de introducción al lenguaje de programación python
+'''
+# Ejemplo de asignación múltiple
+num1,num2=5,10 # asignación de los valores
+num3,num4=9.14,"chay"
+print(num1,num2,num3,num4)
+
+num5,num6 = num1+num2,num1-num2
+print(num5,num6)
+
+#Ejemplo de  asignación concatenada
+num7=num8=num9=10
+print(num7)
+
+#Intercambio de variables
+num10,num11 ="alberto","geto"
+print(num10,num11)
+num11,num10=num10,num11
+print(num10,num11)
+
+nombre,apellido=input("ingresa nombre: "),input("ingresa apellido: ")
