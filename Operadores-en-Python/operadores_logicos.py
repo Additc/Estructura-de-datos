5ba198d0418da540c1e6ac8d5ab84b5b053f0323
AddiSC2 <chavezaddi12@gmail.com>
2024-10-17 16:13:15 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

--- Operadores-en-Python/operadores_logicos.py
+++ Operadores-en-Python/operadores_logicos.py
@@ -2,7 +2,7 @@
 Addi Toro Chavez
 16 de octubre de 2024.
 Descripción:
-Ejercicio de operadores relacionales en python
+Ejercicio de operadores lógicos en python
 '''
 expresion1, expresion2=input("¿te  gusta el futbol? si/no: ") , input("te gusta el basquetbol si/no: ")
 expresion1 = expresion1.lower() == "si" # condicion para devolver true o false
