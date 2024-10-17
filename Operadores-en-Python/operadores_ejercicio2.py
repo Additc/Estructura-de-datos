05914e701519de37e9e4e7f4ca1bc933db0d262f
AddiSC2 <chavezaddi12@gmail.com>
2024-10-17 16:24:19 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

---
+++ Operadores-en-Python/operadores_ejercicio2.py
@@ -0,0 +1,12 @@
+'''
+Addi Toro Chavez
+17 de octubre de 2024.
+Descripción:
+Ejercicio número 2 python
+'''
+
+expresion1, expresion2=input("¿Es profesor de la UNSIJ? si/no: ") , input("Es estudiante de la UNSIJ si/no: ")
+expresion1 = expresion1.lower() == "si" # condicion para devolver true o false
+expresion2 = expresion2.lower() == "si"
+
+print(f"¿Forma parte de la comunidad UNSIJ?: {expresion1 or expresion2}")
---
+++ Operadores-en-Python/operadores_ejercicio3.py
@@ -0,0 +1,8 @@
+'''
+Addi Toro Chavez
+17 de octubre de 2024.
+Descripción:
+Ejercicio número 3 python
+'''
+numero=int(input("ingrese número: "))
+print(f"El número {numero} esta entre 10 y 15? {numero>= 10 and numero<= 15 }")
