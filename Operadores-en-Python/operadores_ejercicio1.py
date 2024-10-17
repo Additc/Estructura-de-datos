f288300e245c4f9ad28ec8b2862b76f4a5296992
AddiSC2 <chavezaddi12@gmail.com>
2024-10-17 16:11:27 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

--- Operadores-en-Python/operadores_ejercicio1.py
+++ Operadores-en-Python/operadores_ejercicio1.py
@@ -9,3 +9,4 @@ compra=float(input("ingrese la cantidad de compra: "))
 expresion1=input("¿compras a meses sin intereses? si/no: ")
 expresion1 = expresion1.lower() == "si" # condicion para devolver true o false
 print(f"¿Aplica bonificación?{ compra >= 5000 and expresion1}")
+print()
