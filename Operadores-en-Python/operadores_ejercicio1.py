d5fd0250820f6a42d4a90fc89e15f123868efd73
AddiSC2 <chavezaddi12@gmail.com>
2024-10-17 16:05:47 -0600

    Se añadieron los archivos de tipos de datos en python y el ejercicio elaborado

---
+++ Operadores-en-Python/operadores_ejercicio1.py
@@ -0,0 +1,11 @@
+'''
+Addi Toro Chavez
+17 de octubre de 2024.
+Descripción:
+Ejercicio de operadores relacionales en python
+'''
+
+compra=float(input("ingrese la cantidad de compra: "))
+expresion1=input("¿compras a meses sin intereses? si/no: ")
+expresion1 = expresion1.lower() == "si" # condicion para devolver true o false
+print(f"¿Aplica bonificación?{ compra >= 5000 and expresion1}")
--- Operadores-en-Python/operadores_logicos.py
+++ Operadores-en-Python/operadores_logicos.py
@@ -5,7 +5,12 @@ Descripción:
 Ejercicio de operadores relacionales en python
 '''
 expresion1, expresion2=input("¿te  gusta el futbol? si/no: ") , input("te gusta el basquetbol si/no: ")
-expresion1 = expresion1.lower() == "si"
+expresion1 = expresion1.lower() == "si" # condicion para devolver true o false
 expresion2 = expresion2.lower() == "si"
+print(expresion1)
+print(expresion2)
 
-print(expresion1, expresion2)
+print(f"¿Ambas respuestas son verdaderas? {expresion1 and expresion2}")
+print(f"¿Una respuesta fue verdadera? {expresion1 or expresion2}")
+print(f"La negación de la primera respuesta es: { not expresion1}")
+print(f"La negación de la segunda respuesta es: { not expresion2}")
