9af8d1d436acd6fac41d522243f94f4365b989a4
Addilap <chavezaddi12@gmail.com>
2024-10-27 15:21:49 -0600

    Sentencia elif en python

--- Sentencias_de_control/Sentencia_if_elif_else.py
+++ Sentencias_de_control/Sentencia_if_elif_else.py
@@ -6,14 +6,18 @@ Sentencia de condición if else if else en python
 '''
 
 #Programa que determine elgrupo de edad al que pertenece
-edad=int(input("Ingrese la edad que tiene: "))
-if edad <= 14 :
-    print("niños y adolecentes")
-elif edad>=15 and edad<=25:
+
+edad=int(input("Ingrese la edad que tiene: "))# Se ingresa la edad.
+if edad <= 14 :# Se establece la primer condición de una determinada edad.
+    print("niños y adolecentes")# Si esta primera condición se cumple se muestra en pantalla.
+
+ # En caso contrario si no se cumple la condición anterior
+# Se establecen varias condiciones, hasta que la edad entre en una de ellas , para posteriormente mostrarlo en pantalla
+elif edad>=15 and edad<=25: # Se establece otra condición con otro rango de edad.
     print("jovenes")
-elif edad>=26 and edad<=45:
+elif edad>=26 and edad<=45:# Se establece otra condición con otro rango de edad.
     print("adultos jovenes")
-elif edad>=46 and edad<60:
+elif edad>=46 and edad<60:# Se establece otra condición con otro rango de edad.
     print("adultos maduros")
-elif edad>=60:
+elif edad>=60:# Se establece otra condición con otro rango de edad.
     print("adultos mayores")
