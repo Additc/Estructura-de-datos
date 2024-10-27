99d77f731f589823448d3b62e32f1a84366c2de3
Addilap <chavezaddi12@gmail.com>
2024-10-27 14:47:25 -0600

    Sentencia if-else en python

--- Sentencias_de_control/sentencia_if_else.py
+++ Sentencias_de_control/sentencia_if_else.py
@@ -7,8 +7,10 @@ Sentencia de condición if else en python
 
 #else si la primera condición no es verdadera
 #Programa que determina si un numero es par o impar
-numero=int(input("ingrese un numero: "))
-if numero % 2==0:
-    print(f"el numero {numero} es par")
-else:
-    print("es impar")
+numero=int(input("ingrese un numero: ")) # ingresamos el número
+
+if numero % 2==0: #Establecemos la condición,  si el módulo del número es 0 .
+    print(f"el numero {numero} es par") # Mostrará en pantalla que es un número par, si la primera condición se cumple.
+
+else: # En caso contrario que la primera condición no se cumpla.
+    print("es impar") # Muestra en pantalla que es un número impar.
