307a75800ebfdf20c9f150751a5e7d80f1faa068
Addilap <chavezaddi12@gmail.com>
2024-12-07 17:47:12 -0600

    Diccionarios ejercicio

--- funcion_y_colecciones/diccionarios_ej1_piedra_papel_tijeras.py
+++ funcion_y_colecciones/diccionarios_ej1_piedra_papel_tijeras.py
@@ -4,12 +4,14 @@ Fecha: 4 de diciembre del 2024
 Descripción:
 Diccionarios ejercicio 1 piedra, papel, tijera.
 """
-import random
+from random import choice # Importó la librería random
+
+#De claro los contadores que llevarán el conteo del juego
 contador_jugador=0
 contador_cpu=0
-contado_empate=0
-diccionario_juego={'PIEDRA':"piedra",'PAPEL':"papel",'TIJERA':"tijera",'JUGADOR':"Gana el jugador",'EMPATE':"Empate",'CPU':"Gana la cpu"}
+contador_empate=0
 
+#Defino la función del menú principal con sus respectivas opciones
 def menu():
     print()
     print("**Juego de piedra, papel y tijera**")
@@ -17,39 +19,68 @@ def menu():
     print("1) Piedra")
     print("2) Papel")
     print("3) Tijera")
-    opcion=input("Ingrese una opcion: ")
+    print("0) Salir")
+    opcion=int(input("Ingrese una opcion: "))
     return opcion
 
-def jugador (opcion, diccionario_juego):
+#Defino la función de las opciones del usuario y del cpu
+def jugador (opcion):
     if opcion == 1:
-        eleccion_usuario = diccionario_juego.get('PIEDRA')
+        eleccion_usuario = "PIEDRA"
     elif opcion == 2:
-        eleccion_usuario = diccionario_juego.get('PAPEL')
+        eleccion_usuario="PAPEL"
     elif opcion == 3:
-        eleccion_usuario = diccionario_juego.get('TIJERA')
-    else:
-        print("Accion incorrecta, intente de nuevo")
-    eleccion_cpu=random.choice(['PIEDRA','PAPEL','TIJERA'])
+        eleccion_usuario = "TIJERA"
+    eleccion_cpu=choice(["PIEDRA","PAPEL","TIJERA"])
     return eleccion_usuario,eleccion_cpu
 
-def logica_juego(diccionario_juego,eleccion_usuario,eleccion_cpu,contador_jugador,contador_cpu,contador_empate):
-    piedra_pape_tijeras={('PIEDRA','TIJERAS'):"JUGADOR",
+#Declaro un ciclo (while) que estará repitiendose mientras el usuario elija la opción que desee
+op=1
+while op != 0:
+    opcion = menu()
+    if opcion < 0 or opcion > 3:
+        print("Opción no válida")
+        print()
+        opcion=menu()
+    if opcion == 0:
+        print("El juego termino")
+        break
+    #Se define el diccionario, que establece la lógica del juego.
+    piedra_papel_tijeras={('PIEDRA','TIJERA'):"JUGADOR",
                          ('PIEDRA','PAPEL'):"CPU",
-                         ('TIJERAS','PAPEL'):"JUGADOR",
-                         ('TIJERAS','PIEDRA'):"CPU",
+                         ('TIJERA','PAPEL'):"JUGADOR",
+                         ('TIJERA','PIEDRA'):"CPU",
                          ('PAPEL','PIEDRA'):"JUGADOR",
-                         ('PAPEL','TIJERAS'):"CPU"}
-    resultado=piedra_pape_tijeras.get((eleccion_usuario,eleccion_cpu))
+                         ('PAPEL','TIJERA'):"CPU"}
+    #Se obtiene la elección del jugador, y del CPU
+    eleccion_usuario, eleccion_cpu = jugador(opcion)
+
+    #Se obtiene el resultado
+    resultado=piedra_papel_tijeras.get((eleccion_usuario,eleccion_cpu),'EMPATE')
+
+    #Se establecen las condiciones, comparando el resultado
     if resultado == "JUGADOR":
+        #Gana el jugador e incremento su contador
         contador_jugador+=1
+        #Muestro en pantalla los resultados del juego
         print(f"Jugador: {eleccion_usuario}, CPU: {eleccion_cpu}. El resultado es: {resultado}")
+        print("Gana el jugador")
+        #Muestro los resultados acumulados
         print(f"Victorias del jugador:{contador_jugador}. Victorias del CPU: {contador_cpu}. Empates:{contador_empate}")
     elif resultado == "CPU":
+        # Gana el CPU e incremento su contador
         contador_cpu += 1
+        # Muestro en pantalla los resultados del juego
         print(f"Jugador: {eleccion_usuario}, CPU: {eleccion_cpu}. El resultado es: {resultado}")
+        print("Gana el CPU")
+        # Muestro los resultados acumulados
         print(f"Victorias del jugador:{contador_jugador}. Victorias del CPU: {contador_cpu}. Empates:{contador_empate}")
     elif resultado == "EMPATE":
+        # El reultado es empate e incremento su contador
         contador_empate+= 1
+        # Muestro en pantalla los resultados del juego
         print(f"Jugador: {eleccion_usuario}, CPU: {eleccion_cpu}. El resultado es: {resultado}")
+        print("Empate")
+        # Muestro los resultados acumulados
         print(f"Victorias del jugador:{contador_jugador}. Victorias del CPU: {contador_cpu}. Empates:{contador_empate}")
-    
+
