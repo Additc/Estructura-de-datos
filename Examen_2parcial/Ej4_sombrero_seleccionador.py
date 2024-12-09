171b659e89b83d5828fb1f0b4ef669f143c9e56c
Addilap <chavezaddi12@gmail.com>
2024-12-08 23:52:14 -0600

    Exámen 2do parcial_ejercicios

--- Examen_2parcial/Ej4_sombrero_seleccionador.py
+++ Examen_2parcial/Ej4_sombrero_seleccionador.py
@@ -4,7 +4,9 @@ Fecha: 7 de diciembre del 2024
 Descripción:
 Ejercicio 3  exámen, Sombrero seleccionador
 """
+from random import choice
 def menu():
+    print()
     print("Test del sombrero seleccionador de Harry Potter")
     print("1) Iniciar test")
     print("2) Salir ")
@@ -28,29 +30,66 @@ def test():
     for pregunta in Diccionario_de_preguntas['pregunta1']:
         contador +=1
         print(f"{contador}) {pregunta}")
-    opcion = int(input("\nSelecciona una opción :"))
+    opcion1 = int(input("Selecciona una opción :"))
+    print()
+
 
     print("Después de tu muerte ¿qué es lo que más le gustaría que hiciera la gente cuando escuche su nombre?")
     contador = 0
     for pregunta in Diccionario_de_preguntas['pregunta2']:
         contador += 1
         print(f"{contador}) {pregunta}")
-    opcion2 = int(input("\nSelecciona una opción :"))
+    opcion2 = int(input("Selecciona una opción :"))
+    print()
+
 
     print("Dada la opción, preferirías inventar una poción que garantizara:")
     contador = 0
     for pregunta in Diccionario_de_preguntas['pregunta3']:
         contador += 1
         print(f"{contador}) {pregunta}")
-    opcion3= int(input("\nSelecciona una opción :"))
+    opcion3= int(input("Selecciona una opción :"))
+    print()
+
 
     print("¿Cómo te definirías en una sola palabra?")
     contador = 0
-
     for pregunta in Diccionario_de_preguntas['pregunta4']:
         contador += 1
-        print(f"{contador}) {pregunta}\n")
-    opcion4 = int(input("\nSelecciona una opción :"))
+        print(f"{contador}) {pregunta}")
+    opcion4 = int(input("Selecciona una opción :"))
+    print()
+
+
+    print("¿Qué cualidad te describe mejor?")
+    contador = 0
+    for pregunta in Diccionario_de_preguntas['pregunta5']:
+        contador += 1
+        print(f"{contador}) {pregunta}")
+    opcion5 = int(input("Selecciona una opción :"))
+    print()
+
+
+    print("¿Cuál es tu clase favorita?")
+    contador = 0
+    for pregunta in Diccionario_de_preguntas['pregunta6']:
+        contador += 1
+        print(f"{contador}) {pregunta}")
+    opcion6 = int(input("Selecciona una opción :"))
+    print()
+
+
+    print("¿Cuál es tu lenguaje de programación favorito?")
+    contador = 0
+    for pregunta in Diccionario_de_preguntas['pregunta7']:
+        contador += 1
+        print(f"{contador}) {pregunta}")
+    opcion7 = int(input("Selecciona una opción :"))
+    print()
+
+
+
+
 
 op = 1
 while op != 0:
