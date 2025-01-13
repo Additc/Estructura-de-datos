48e3fd1c860d78b36d6ef71bfc70ab24e3fc93e2
AddiSC2 <chavezaddi12@gmail.com>
2025-01-13 17:12:31 -0600

    saludar main

---
+++ Buenas_practicas/saludar_main.py
@@ -0,0 +1,10 @@
+'''
+Nombre: Addi Toro Chavez
+Fecha: 8 de enero de 2025.
+Descripción: Saludar main
+'''
+from saludar_modulo import saludar
+
+if __name__ == '__main__':
+    nombre = input("ingrese nombre: ")
+    saludar(nombre)
